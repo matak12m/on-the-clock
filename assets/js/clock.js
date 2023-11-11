80230feeb2dd3e86d9f47e4d505b2220b1f300d5
(Student  C00288248) Matej Martinek <C00288248@setu.ie>
2023-11-11 15:06:34 +0000

    fully functional interactive clock

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -4,6 +4,14 @@ let clockContainer = document.getElementById("clockContainer");
 
 clockContainer.addEventListener("mousedown", mouseDown);
 clockContainer.addEventListener("mouseup", mouseUp)
+clockHand.addEventListener("mouseover", preventRotation);
+clockHand.addEventListener("mouseout", preventRotationStop)
+
+let canRotate = 1;
+let degrees = -90; //global so it can be read by the gameplay script.
+
+
+
 
 
 //these two functions enable/disabe rotation of the clock hand.
@@ -16,23 +24,52 @@ function mouseUp() {
 }
 
 
-//calculates the angle for the clockhand to rotate to, and rotates it.
+
+//the coordinates from getAngleDegrees are from the element that the mouse is hovering over. if the mouse is over the clockHand, it would rotate it to the incorrect place. these functions prevent that.
+function preventRotation() {
+    canRotate = 0;
+}
+
+function preventRotationStop() {
+    canRotate = 1;
+}
+
+
+
+
+
+
+
+//rotates the clockhand
 function rotate() {
-    let mouseX = event.offsetX;
-    let mouseY = event.offsetY;
+
+    if (canRotate == 1) {
+        let rotateIndex = getAngleDegrees();
+
+        clockHand.style.rotate = rotateIndex;
+    }
+  
+
+    }
+
+//calculates the angle the clockhand should be rotated to.
+function getAngleDegrees() {
     const clockCenterX = 100;
     const clockCenterY = 100;
 
-    let difX = mouseX - clockCenterX;
+    let mouseX = event.offsetX;
+    let mouseY = event.offsetY;
 
-    let y = Math.sin(difX) * Math.cos(clockCenterY);
-    let x = Math.cos(mouseY) * Math.sin(clockCenterY) - Math.sin(mouseY) * Math.cos(clockCenterY) * Math.cos(difX);
+    if ((mouseX < 95 || mouseX > 105) && (mouseY < 95 || mouseY > 105)) { //prevents rapid jumping of the clock through moving through the center
 
-    let angle = Math.atan2(y, x);
+        let deltaX = clockCenterX - mouseX;
+        let deltaY = clockCenterY - mouseY;
+        let radians = Math.atan2(deltaY, deltaX)
+        degrees = ((radians * 180) / Math.PI) - 90;
 
-    console.log(angle);
-
-
+        degrees = (degrees + 360) % 360;
     }
-    
+    console.log('degrees:', {degrees })
+    return degrees + "deg";
+}
 
