ef5d8a5954d637914340d604487f2e948b91b4bc
(Student  C00288248) Matej Martinek <C00288248@setu.ie>
2023-11-28 12:42:04 +0000

    UI changes, controls text

--- assets/js/textbox.js
+++ assets/js/textbox.js
@@ -7,6 +7,18 @@ let mainTextBox = document.getElementById("mainTextBox")
 let textChoice1 = document.getElementById("textChoice1")
 let textChoice2 = document.getElementById("textChoice2")
 let textChoice3 = document.getElementById("textChoice3")
+let controlsBox = document.getElementById("controls")
+let closeControlsButton = document.getElementById("close-button")
+
+
+closeControlsButton.addEventListener("click", () => { closeControls() })
+
+
+function closeControls() {
+    controlsBox.style.visibility = "hidden";
+    closeControlsButton.style.visibility = "hidden";
+
+}
 
 
 
