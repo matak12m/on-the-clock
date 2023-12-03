c3cf176a1a5f31a07849f5dd50bfb307189b1e35
unknown <C00288248@setu.ie>
2023-12-03 21:07:27 +0000

    index page improved

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,14 @@
+let mobileButton = document.getElementById("mobileButton")
+let browserButton = document.getElementById("browserButton")
 
+
+mobileButton.addEventListener("click", goToMobile);
+browserButton.addEventListener("click", gotoBrowser);
+
+function goToMobile() {
+    window.location.replace("mobileGameplay.html");
+}
+
+function gotoBrowser () {
+    window.location.replace("browserGameplay.html");
+}
