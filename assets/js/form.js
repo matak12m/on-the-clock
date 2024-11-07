9b92d0dfc21eb49d2e572a6cd12de5cb1a78287c
unknown <matak12m@seznam.cz>
2024-11-07 15:28:56 +0000

    tweaked for browser demo deployment

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,13 +1,13 @@
-let mobileButton = document.getElementById("mobileButton")
+//let mobileButton = document.getElementById("mobileButton")
 let browserButton = document.getElementById("browserButton")
 
 
-mobileButton.addEventListener("click", goToMobile);
+//mobileButton.addEventListener("click", goToMobile);
 browserButton.addEventListener("click", gotoBrowser);
 
-function goToMobile() {
-    window.location.replace("mobileGameplay.html");
-}
+// function goToMobile() {
+//     window.location.replace("mobileGameplay.html");
+// }
 
 function gotoBrowser () {
     window.location.replace("browserGameplay.html");
